edaa19a5155e7ae9d861742a5750f88da23d7cb1
LuriaReimeiko <119699697+LuriaReimeiko@users.noreply.github.com>
2025-11-21 14:07:46 +0100

    Comments

--- include/QSFML.hpp
+++ include/QSFML.hpp
@@ -4,15 +4,21 @@
 #include <QWidget>
 #include <SFML/Graphics/RenderWindow.hpp>
 
+// Inherit from QWidget to place on the QApplication
+// Inherit from sf::RenderWindow for less redundancy compared to storing one
 class QSFML : public QWidget, public sf::RenderWindow
 {
 public:
     explicit QSFML(QWidget* parent = nullptr);
 
 protected:
+    // Used as initialization call
     void showEvent(QShowEvent* event) override;
+
+    // Update calls to the SFML render window
     void paintEvent(QPaintEvent* event) override;
 
+    // Disable Qt5's paint engine
     QPaintEngine* paintEngine() const override;
 
 private:
